abce33246e33aba88b4fe90d6ceff9985afe0fd1
TommyEvertsen <tommyevertsen52@gmail.com>
2025-04-11 12:43:37 +0200

    Made some units and started battlesystem

--- src/App.java
+++ src/App.java
@@ -1,9 +1,15 @@
-import armyBuilder.UnitsArmyBuilder.RatClanMan;
+import armyBuilder.Battle.Battle;
+import armyBuilder.UnitsArmyBuilder.Orcs.OrcGrunt;
+import armyBuilder.UnitsArmyBuilder.Rats.RatClanMan;
 
 public class App {
     public static void main(String[] args) throws Exception {
      
 RatClanMan rat = new RatClanMan();
 rat.getStatus();
+OrcGrunt orc = new OrcGrunt();
+orc.getStatus();
+Battle battle = new Battle();
+battle.fight(rat, orc);
 }
 }
--- src/armyBuilder/UnitsArmyBuilder/Orcs/OrcGrunt.java
+++ src/armyBuilder/UnitsArmyBuilder/Orcs/OrcGrunt.java
@@ -6,22 +6,136 @@ import armyBuilder.EnumArmyBuilder.Type;
 import armyBuilder.EnumArmyBuilder.Weaknesses;
 import armyBuilder.UnitsArmyBuilder.Unit;
 
+
 public class OrcGrunt extends Unit {
 
-    private final String type = Type.ORC.name();
+    private  String type = Type.ORC.name();
     private int hp = 16;
     private int strenght = 7;
     private int agility = 2;
     private int defence = 3;
     private int range = 1;
     private int cost = 150;
-    private final String[] weaknesses = {Weaknesses.LIGHTNING.name()};
-    private final String[] resistances = {Resistances.FIRE.name(), Resistances.ICE.name()};
-    private final String[] traits = {Traits.EARTH.name()};
+    private boolean firstStrike = false;
+    private  String[] weaknesses = {Weaknesses.LIGHTNING.name()};
+    private  String[] resistances = {Resistances.FIRE.name(), Resistances.ICE.name()};
+    private  String[] traits = {Traits.EARTH.name()};
    
 
     public OrcGrunt() {
       
+    }
+
+    @Override
+    public String getType() {
+        return type;
+    }
+
+    @Override
+    public int getHp() {
+        return hp;
+    }
+
+    @Override
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    @Override
+    public int getStrenght() {
+        return strenght;
+    }
+
+    @Override
+    public int setStrenght(int strenght) {
+      return this.strenght = strenght;
+    }
+
+    @Override
+    public int getAgility() {
+        return agility;
+    }
+
+    @Override
+    public void setAgility(int agility) {
+        this.agility = agility;
+    }
+
+    @Override
+    public int getDefence() {
+        return defence;
+    }
+
+    @Override
+    public void setDefence(int defence) {
+        this.defence = defence;
+    }
+
+    @Override
+    public String[] getWekanesses() {
+        return wekanesses;
+    }
+    
+    @Override
+    public void setWekanesses(String[] wekanesses) {
+        this.wekanesses = wekanesses;
+    }
+    
+    @Override
+    public String[] getTraits() {
+        return traits;
+    }
+
+    @Override
+    public void setTraits(String[] traits) {
+        this.traits = traits;
+    }
+
+@Override
+    public int getCost() {
+        return cost;
+    }
+
+    @Override
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    @Override
+    public boolean getFirstStrike(){
+        return firstStrike;
+    }
+
+    @Override
+    public void setFirstStrike(boolean firstStrike){
+        this.firstStrike = firstStrike;
+    }
+
+    @Override
+    public int getRange(){
+        return range;
+    }
+    @Override
+    public void setRange(int range){
+        this.range = range;
+    }
+
+
+    // Cusom methods
+    @Override
+    public void attack(Unit target) {
+        int damage = this.strenght - target.getDefence();
+        if (damage > 0) {
+            target.setHp(target.getHp() - damage);
+        }
+    }
+
+    @Override
+    public void defend(int damage) {
+        int actualDamage = damage - this.defence;
+        if (actualDamage > 0) {
+            this.hp -= actualDamage;
+        }
     }
 
     @Override
