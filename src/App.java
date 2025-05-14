48462427f666f22dd65b1b402351f5c1439e68b7
TommyEvertsen <tommyevertsen52@gmail.com>
2025-05-14 12:02:10 +0200

    Added more stuff

--- src/App.java
+++ src/App.java
@@ -1,10 +1,26 @@
 import armyBuilder.Battle.Battle;
 import armyBuilder.UnitsArmyBuilder.Orcs.OrcGrunt;
 import armyBuilder.UnitsArmyBuilder.Rats.RatClanMan;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class App {
     public static void main(String[] args) throws Exception {
+
+
+
+    /*  Game window */
+       JFrame frame = new JFrame("Armybuilder");
+       JLabel label = new JLabel("Geeks Premier League 2023");
+
+       frame.add(label);
+       frame.setSize(800, 800);
+       frame.setExtendedState(frame.MAXIMIZED_BOTH);
+       frame.setVisible(true);
+       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
      
+
+
 RatClanMan rat = new RatClanMan();
 
 OrcGrunt orc = new OrcGrunt();
---
+++ src/armyBuilder/BattleFrame.java
@@ -0,0 +1,13 @@
+package armyBuilder;
+
+
+
+import java.awt.Frame;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class BattleFrame {
+
+
+
+}
